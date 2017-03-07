b499cffab9933a109120931597c92251c543bbc8
Jay Prakash <rajasachin130@gmail.com>
2017-03-07 22:02:59 +0530

    added file1.cpp

---
+++ file1.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<stdlib.h>
+  using namespace std;
+ class person
+   {
+     int age;
+      char *name;
+      public :
+      person()
+       {
+          age=19;
+          name="JAy Prakash";
+        }
+      };
+   int main()
+     {
+       cout<<"Doing something...";
+      }
